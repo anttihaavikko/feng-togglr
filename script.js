d83d40adfba3276b652567a96df8fda16479a74f
Antti Haavikko <antti.haavikko@focus.fi>
2016-02-24 12:15:43 +0200

    Add copy task button

--- script.js
+++ script.js
@@ -14,7 +14,21 @@ $.start = function() {
   if (feng_url && feng_url.length > 0 && window.location.toString().indexOf(feng_url) > -1) {
     $.tryToAddButton = function() {
       if($("#actionsDialog1").length > 0 && $("#toggl-button").length == 0) {
+
+        var task_id = $(".prop-col-div").eq(0).text().split(":")[1].trim();
+        var task_name = $(".coViewTitleContainer td").eq(0).text().trim();
+
+        $("#actionsDialog1").prepend("<a href='#' id='copy-button' data-clipboard-text='"+task_id+": "+task_name+"'>Copy task</a>");
         $("#actionsDialog1").prepend("<a href='#' id='toggl-button'>Toggl this</a>");
+
+        var clipboard = new Clipboard('#copy-button');
+        clipboard.on('success', function(e) {
+          $('#copy-button').addClass("done");
+          setTimeout(function() {
+            $('#copy-button').removeClass("done");
+          }, 2000);
+          e.clearSelection();
+        });
       }
 
       setTimeout($.tryToAddButton, 1000);
@@ -29,19 +43,21 @@ $.start = function() {
       var task_id = $(".prop-col-div").eq(0).text().split(":")[1].trim();
       var task_name = $(".coViewTitleContainer td").eq(0).text().trim();
       var project_name = $(".mname").eq(0).text().trim();
+      var task_data = '{"time_entry":{"description":"'+task_id+": "+task_name+'","created_with":"Feng Togglr", "tags":["'+project_name+'"]}}';
 
       $.ajax({
         url: 'https://www.toggl.com/api/v8/time_entries/start',
         method: 'POST',
         username: api_token,
         password: 'api_token',
-        headers: {
-          "Content-Type": "application/json"
-        },
-        data: '{"time_entry":{"description":"'+task_id+": "+task_name+'","created_with":"Feng Togglr", "tags":["'+project_name+'"]}}'
+        headers: {"Content-Type": "application/json"},
+        data: task_data
       }).done(function() {
         $("#toggl-button").slideUp("fast");
-      })
+      }).fail(function() {
+        console.log("API token: " + api_token);
+        console.log("DATA: " + task_data);
+      });
     });
   }
 }
